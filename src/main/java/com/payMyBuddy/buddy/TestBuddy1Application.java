d92ee28416c46c68b0f92c9bb864f1f257602604
aurelia marchand <aurelia.marchand.95@gmail.com>
2021-04-23 14:32:41 +0200

    first commit

---
+++ src/main/java/com/payMyBuddy/buddy/TestBuddy1Application.java
@@ -0,0 +1,13 @@
+package com.payMyBuddy.buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBuddy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBuddy1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/payMyBuddy/buddy/TestBuddy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payMyBuddy.buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBuddy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
